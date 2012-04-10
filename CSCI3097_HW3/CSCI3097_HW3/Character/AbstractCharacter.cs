399dafd21a7271df0cce6bc3870e18d6928a5687
Kyle <kwhittin@uno.edu>
2012-04-09 21:27:14 -0500

    adding Character, AbstractCharacter and Player

--- CSCI3097_HW3/CSCI3097_HW3/Character/AbstractCharacter.cs
+++ CSCI3097_HW3/CSCI3097_HW3/Character/AbstractCharacter.cs
@@ -3,21 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace CSCI3097_HW3.Character
 {
   /// <summary>
-  /// Will model a character that would
-  /// exist on and interact with a level object.
+  /// Will contain and implement all common functionality
+  /// between the separte concrete instantiations of a character.
   /// </summary>
-  interface Character
+  abstract class AbstractCharacter : Character
   {
-    /// <summary>
-    /// Will return the velocity of this charcter
-    /// at the moment the method is called.
-    /// ENSURE:   this.speed is returned
-    /// </summary>
-    Vector2 currentSpeed();
+    //INSTANCE VARIABLES
+    protected Texture2D texture;
+    protected Vector2 current_velocity;
+    protected float walk_velocity;
+    protected float run_velocity;
+    protected float jump_velocity;
+    protected bool is_jumping;
+    protected bool is_running;
+    protected Vector2 position;
+
+    /////////////////////////////////////////////////////////////////
+    #region QUERIES
 
     /// <summary>
     /// Will return the hit box for this character,
@@ -25,7 +32,22 @@ namespace CSCI3097_HW3.Character
     /// has to occur in order to register on this character.
     /// ENSURE:   a Rectangle with X, Y, Width and Height > 0 is returned
     /// </summary>
-    Rectangle BoundingBox();
+    public Rectangle BoundingBox()
+    {
+      //return a new rectagle based on this character's
+      //current position and width and height
+      return new Rectangle((int)this.position.X, (int)this.position.Y, this.texture.Width, this.texture.Height);
+    }
+
+    /// <summary>
+    /// Will return the velocity of this charcter
+    /// at the moment the method is called.
+    /// ENSURE:   this.speed is returned
+    /// </summary>
+    public Vector2 currentSpeed()
+    {
+      return this.current_velocity;
+    }
 
     /// <summary>
     /// Will return whether or not the character is currently walking.
@@ -34,7 +56,11 @@ namespace CSCI3097_HW3.Character
     ///           otherwise,
     ///            return false
     /// </summary>
-    bool isWalking();
+    public bool isWalking()
+    {
+      //return the opposite of is_running
+      return this.is_running.Equals(false);
+    }
 
     /// <summary>
     /// Will return whether or not the character is currently running.
@@ -43,7 +69,11 @@ namespace CSCI3097_HW3.Character
     ///           otherwise,
     ///            return false
     /// </summary>
-    bool isRunning();
+    public bool isRunning()
+    {
+      //return is_running
+      return this.is_running;
+    }
 
     /// <summary>
     /// Will return whether or not the character is currently jumping.
@@ -52,7 +82,17 @@ namespace CSCI3097_HW3.Character
     ///           otherwise,
     ///            return false
     /// </summary>
-    bool isJumping();
+    public bool isJumping()
+    {
+      //return is_jumping
+      return this.is_jumping;
+    }
+
+    #endregion QUERIES
+    /////////////////////////////////////////////////////////////////
+
+    /////////////////////////////////////////////////////////////////
+    #region COMMANDS
 
     /// <summary>
     /// Will move this character to the left of their current position.
@@ -64,7 +104,10 @@ namespace CSCI3097_HW3.Character
     ///            new.position.X is greater or equal to
     ///            old.position.X-horizontal velocity
     /// </summary>
-    void moveLeft();
+    public void moveLeft()
+    {
+      this.position.X = this.position.X - this.current_velocity.X;
+    }
 
     /// <summary>
     /// Will move this character to the right of their current position.
@@ -76,7 +119,10 @@ namespace CSCI3097_HW3.Character
     ///            new.position.X is less or equal to
     ///            old.position.X-horiztonal velocity
     /// </summary>
-    void moveRight();
+    public void moveRight()
+    {
+      this.position.X = this.position.X + this.current_velocity.X;
+    }
 
     /// <summary>
     /// Will make this character jump, giving it a brief veritcal velocity.
@@ -85,7 +131,13 @@ namespace CSCI3097_HW3.Character
     /// ENSURE:   this character will be givin a vertical velocity to
     ///            simulate jumping into the air.
     /// </summary>
-    void jump();
+    public void jump()
+    {
+      //turn the flag on
+      this.is_jumping = true;
+      //apply a negative force to Y (up is negative, down positive)
+      this.current_velocity.Y = (0 - this.jump_velocity);
+    }
 
     /// <summary>
     /// Will make this character run, setting its current velocity to
@@ -94,7 +146,13 @@ namespace CSCI3097_HW3.Character
     /// The speed will remain constant.
     /// ENSURE:   this character's velocity will be set to its running velocity
     /// </summary>
-    void run();
+    public void run()
+    {
+      //turn the flag on
+      this.is_running = true;
+      //set the current horizontal velocity to the running velocity
+      this.current_velocity.X = this.run_velocity;
+    }
 
     /// <summary>
     /// Will make this character walk, setting its current velocity to
@@ -103,6 +161,15 @@ namespace CSCI3097_HW3.Character
     /// The speed will remain constant.
     /// ENSURE:   this character's velocity will be set to it walking velocity
     /// </summary>
-    void walk();
+    public void walk()
+    {
+      //turn the flag off
+      this.is_running = false;
+      //set the current horizontal velocity to the walking velocity
+      this.current_velocity.X = this.walk_velocity;
+    }
+
+    #endregion COMMANDS
+    /////////////////////////////////////////////////////////////////
   }
 }
---
+++ CSCI3097_HW3/CSCI3097_HW3/Character/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CSCI3097_HW3.Character
+{
+  /// <summary>
+  /// Will model a player character receiving input
+  /// from a source outside of this program but being
+  /// processed by its components.
+  /// </summary>
+  class Player : AbstractCharacter
+  {
+    /// <summary>
+    /// Will create a new instance of this player character with the given
+    /// texture, at the given position, with the given walking, running
+    /// and jumping speeds.
+    /// REQUIRE:  given.texture != null
+    ///            given.position != nil && given.position.X,Y > 0
+    ///            given.walking, running, jumping speed != null && > 0
+    /// ENSURE:   this.currentSpeed() == given.walking_speed
+    ///            this.boundingBox().X,Y == given.position.X,Y
+    /// </summary>
+    public Player(Texture2D texture, Vector2 position, float walk_speed,
+      float run_speed, float jump_speed)
+    {
+      this.texture = texture;
+      this.position = position;
+      this.walk_velocity = walk_speed;
+      this.run_velocity = run_speed;
+      this.jump_velocity = jump_speed;
+      this.current_velocity = new Vector2(walk_speed, 0);
+    }
+  }
+}
